b38caefd0f80b1c84431d852f38a3f7ad1ff454a
Daniel Legan <legan.daniel@gmail.com>
2023-05-05 12:44:04 -0400

    terminal working

--- Start/Overview/MyConsoleApp/Program.cs
+++ Start/Overview/MyConsoleApp/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/*
+auto-added with .NET 6.0
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+*/
+
 Console.WriteLine("Hello, World!");
+
+string response;
+Console.WriteLine("What's your name?");
+response = Console.ReadLine();
+
+Console.WriteLine($"Enjoy the course, {response}!");
