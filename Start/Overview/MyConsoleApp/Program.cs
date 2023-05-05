4c56611178c813d74f5dc962ceab0fd7e2460dc1
Daniel Legan <legan.daniel@gmail.com>
2023-05-05 13:04:14 -0400

    datatypes

--- Start/Overview/MyConsoleApp/Program.cs
+++ Start/Overview/MyConsoleApp/Program.cs
@@ -19,3 +19,7 @@ Console.WriteLine("What's your name?");
 response = Console.ReadLine();
 
 Console.WriteLine($"Enjoy the course, {response}!");
+
+OperatingSystem thisOS = Environment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
